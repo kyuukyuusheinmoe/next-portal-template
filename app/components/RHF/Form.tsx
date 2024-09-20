ca5a50c390b8dea5b103fc07753ddd38c1df7c9f
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2024-09-21 00:08:17 +0700

    set up form for common usecase

--- app/components/RHF/Form.tsx
+++ app/components/RHF/Form.tsx
@@ -1,14 +1,44 @@
 "use client"
 import React from 'react'
 import { FormProvider, useForm } from 'react-hook-form';
+import { FormControlElementProps } from './FormControlElement';
+import FormLabel from '../Typography/FormLabel';
+import clsx from 'clsx';
+import FormControlElement from './FormControlElement';
 
-const Form = () => {
+export type FormElementProps = Omit<FormControlElementProps, "control">
+
+type FormProps = {
+  formStatus?: "Submitting",
+  onSubmit: (data: any) => void,
+  compoenents: FormElementProps[]
+}
+
+
+const Form = ({formStatus, onSubmit, compoenents}: FormProps) => {
     const methods = useForm()
+    const {handleSubmit, control} = methods;
+
   return (
     <FormProvider {...methods}>
-        <form>
-            
+        <form onSubmit={handleSubmit(onSubmit)}>
+        {
+                    compoenents?.map ((component: FormElementProps, index: number) => {
+                    if (component.componentType === "container" && component.components?.length) {
+                            return (<div className='p-4' key={`${component.name}-${index}`}>  
+                                        {component.label && <FormLabel label={component.label}/>}
+                                        <div className={clsx(component.containerClass)}> 
+                                            {component?.components.map ((component, idx)=> <FormControlElement key={`${component.name}-${idx}`} {...component} control={control}  />)}
+                                        </div>
+                                    </div>)
+                    } 
+                    return <div className='p-4' key={`${component.name}-${index}`}> <FormControlElement {...component} control={control}/></div>
+                })
+                }
         </form>
+        <div className="flex justify-end mt-4">
+            <button className="btn btn-primary" disabled={formStatus === "Submitting"}>Submit</button>
+        </div>
     </FormProvider>
   )
 }
