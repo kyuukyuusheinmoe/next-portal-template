2c1200112a27d8bcf2d8ae21315707574cc6c944
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2024-09-20 15:33:24 +0700

    add signin page

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,9 +1,24 @@
-import Login from '@/app/features/user/Login'
+import LoginForm from '@/app/features/user/Login'
 import React from 'react'
 
 const Page = () => {
   return (
-    <Login/>
+    <>
+    <div className="min-h-screen bg-base-200 flex items-center">
+            <div className="card mx-auto w-full max-w-5xl  shadow-xl">
+                <div className="grid  md:grid-cols-2 grid-cols-1  bg-base-100 rounded-xl">
+                <div className="m-auto text-center leading-10 tracking-widest">
+                <div className="text-center mt-12"><img src="/intro.png" alt="Dashwind Admin Template" className="w-48 inline-block"></img></div>
+                    <h5 className="text-4xl"> Pawn Shop Admin Portal</h5>
+                </div>
+                <div className='py-24 px-10'>
+                    <h2 className='text-2xl font-semibold mb-2 text-center'>{"Welcome"}</h2>
+                    <LoginForm />
+                </div>
+            </div>
+            </div>
+        </div>
+    </>
   )
 }
 
--- app/features/user/Login.tsx
+++ app/features/user/Login.tsx
@@ -1,39 +1,48 @@
+"use client"
+import { useState, useTransition } from 'react';
 import Link from 'next/link'
-import InputText from '../../components/Input/InputText'
 import ErrorText from '../../components/Typography/ErrorText';
-import { siginin } from '@/services/authServices';
 
-function Login(){
+const Login = () => {
+    const [isPending, startTransition] = useTransition();
+    const [errorMsg, setErrorMsg] = useState<string | null>(null)
 
+    const onSubmit = (data: FormData) => {
+        startTransition(async () => {
+            // const res = await signin(data)
+            // if (res?.errorMsg) {
+            //     setErrorMsg(res?.errorMsg)
+            // }
+        })
+    }
     return(
-        <div className="min-h-screen bg-base-200 flex items-center">
-            <div className="card mx-auto w-full max-w-5xl  shadow-xl">
-                <div className="grid  md:grid-cols-2 grid-cols-1  bg-base-100 rounded-xl">
-                <div className="m-auto text-center leading-10 tracking-widest">
-                <div className="text-center mt-12"><img src="/intro.png" alt="Dashwind Admin Template" className="w-48 inline-block"></img></div>
-                    <h5 className="text-4xl"> Mortgage Management Admin Portal</h5>
-                </div>
-                <div className='py-24 px-10'>
-                    <h2 className='text-2xl font-semibold mb-2 text-center'>Login</h2>
-                    <form action={siginin}>
+        <>
+        <form action={onSubmit}>
                         <div className="mb-4">
-                            <InputText type="email" containerStyle="mt-4" labelTitle="Email Id" name="email"/>
-
-                            <InputText type="password" containerStyle="mt-4" labelTitle="Password" name="password"/>
+                            <div className={`form-control w-full`}>
+                                <label className="label">
+                                    <span className={"label-text text-base-content "}>{"User Name"}</span>
+                                </label>
+                                <input type="text" className="input input-bordered" name="username"/>
+                            </div>
+                            <div className={`form-control w-full`}>
+                                <label className="label">
+                                    <span className={"label-text text-base-content "}>{"Password"}</span>
+                                </label>
+                                <input type="password" className="input input-bordered" name="password"/>
+                            </div>
                         </div>
 
                         <div className='text-right text-primary'><Link href="/forgot-password"><span className="text-sm  inline-block  hover:text-primary hover:underline hover:cursor-pointer transition duration-200">Forgot Password?</span></Link>
                         </div>
 
-                        <ErrorText styleClass="mt-8">{"No Error"}</ErrorText>
-                        <button type="submit" className={"btn mt-2 w-full btn-primary"}>Login</button>
+                        <ErrorText styleClass="mt-8">{errorMsg}</ErrorText>
+                        <button type="submit" disabled={isPending} aria-disabled={isPending} className={"btn mt-2 w-full btn-primary"}>{"Login"}</button>
 
                         <div className='text-center mt-4'>Don't have an account yet? <Link href="/register"><span className="  inline-block  hover:text-primary hover:underline hover:cursor-pointer transition duration-200">Register</span></Link></div>
                     </form>
-                </div>
-            </div>
-            </div>
-        </div>
+        </>
+        
     )
 }
 
