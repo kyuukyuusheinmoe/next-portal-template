28c5e930756a3ff2b10b2e7a139b16996718e28d
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2024-09-21 00:08:47 +0700

    add cash in form

---
+++ app/constants/transaction/forms.ts
@@ -0,0 +1,41 @@
+import { FormElementProps } from "@/app/components/RHF/Form"
+import { CASH_IN, CASH_OUT } from "../common"
+
+export const CashinForm : FormElementProps[] = [
+    {
+        componentType: 'select',
+        dataType: 'text',
+        name: "service",
+        label: "Service",
+        labelField: "label",
+        valueField: "value",
+        dataSource: {type: "LIST", 
+            options: [{label: "Cash In", value: CASH_IN},
+                {label: "Cash Out", value: CASH_OUT}
+            ]
+        },
+        validations: {
+            required: true
+        }
+    },
+    {
+        componentType: 'select',
+        dataType: 'text',
+        name: "currency",
+        label: "Currency",
+        labelField: "label",
+        valueField: "value",
+        dataSource: {type: "LIST", 
+            options: [{label: 'BTC', value: 'BTC'}, {label: 'USDT', value: 'USDT'}]
+        },
+        validations: {
+            required: true
+        }
+    },
+    {
+        componentType: 'input',
+        dataType: 'text',
+        name: "amount",
+        label: "Amount",
+    }
+]
--- app/features/transaction/DataTable.tsx
+++ app/features/transaction/DataTable.tsx
@@ -1,8 +1,11 @@
 "use client"
-import React from 'react';
+import React, {useState} from 'react';
 import { ColumnProps } from '@/app/types/common';
 import TableGroup from '../common/TableGroup';
-
+import { InboxArrowDownIcon } from '@heroicons/react/24/outline';
+import { CASH_IN, CASH_OUT } from '@/app/constants/common';
+import Dialog from '@/app/components/Dialog/Dialog';
+import FormComponent from './FormComponent';
 
 type DataTableProps = {
     data: {[key: string]: any}[]
@@ -10,11 +13,29 @@ type DataTableProps = {
     totalRows: number
 }
 
+
 function DataTable({data=[], columns=[], totalRows}: DataTableProps){
-    
+    const [isDialogOpen, setIsDialogOpen] = useState<typeof CASH_IN | typeof CASH_OUT | null>(null)
+
+    const cashinDialogOpen = isDialogOpen === CASH_IN;
+
+    const headerActionList = [
+        {
+            label: "Cash In",
+            icon: <InboxArrowDownIcon className='w-6 h-6' />,
+            action: () => setIsDialogOpen(CASH_IN)
+        }
+    ]
+    const handleDialogClose = () => {
+        setIsDialogOpen(null)
+    }
+
     return(
         <>
-            <TableGroup title='Transaction List' columns={columns} data={data} filterComponents={[]}  totalRows={totalRows} rowActionList={[]} headerActionList={[]}/>
+            <TableGroup title='Transaction List' columns={columns} data={data} filterComponents={[]}  totalRows={totalRows} rowActionList={[]} headerActionList={headerActionList}/>
+            <Dialog title='Cash In' isOpen={cashinDialogOpen} onClose={handleDialogClose} >   
+                {cashinDialogOpen && <FormComponent />}
+            </Dialog>
         </>
     )
 }
