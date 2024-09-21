7ab6e4135eb9b5752963ffdc37ac3112ddf3f68d
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2024-09-21 10:46:30 +0700

    add type

---
+++ app/types/transaciton.ts
@@ -0,0 +1,45 @@
+import { account, CASH_IN, CASH_OUT, CREDIT, DEBIT, Fail, FUNDING, peding, pocket, POCKET, settled, Success } from "../constants/common"
+
+export type TransactionResponse = {
+        user: string,
+        amount: number,
+        currency: string,
+        service: typeof CASH_IN | typeof CASH_OUT,
+        type: typeof CREDIT | typeof DEBIT,
+        sender: string,
+        receiver: string,
+        status: typeof settled | typeof peding,
+        creditLog: {
+            walletId: string,
+            user: string,
+            name: typeof POCKET,
+            type: typeof pocket,
+            currency: string,
+            before: {
+                balance: number
+            },
+            after: {
+                balance: number
+            },
+            err: 0,
+            message: typeof Success | typeof Fail
+        },
+        debitLog: {
+            walletId: string,
+            name: typeof FUNDING,
+            type: typeof account,
+            currency: string,
+            before: {
+                balance: number
+            },
+            after: {
+                balance: number
+            },
+            err: 0 | 1,
+            message: string
+        },
+        _id: string,
+        createdAt:Date,
+        updatedAt:Date,
+}
+
