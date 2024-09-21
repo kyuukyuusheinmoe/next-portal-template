68f493a0e11f5c718ad55eeb7e5a1ccf51b96410
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2024-09-21 10:46:21 +0700

    add constants

--- app/constants/common.ts
+++ app/constants/common.ts
@@ -2,3 +2,16 @@ export const amountFields = ["amount", "reimbursedInterest", "reimbursedLoan", "
 
 export const CASH_IN = "CASHIN"
 export const CASH_OUT = 'CASHOUT'
+export const CREDIT = "CREDIT"
+export const DEBIT = "CREDIT"
+export const settled = "settled"  
+export const peding = "peding"  
+export const POCKET = "POCKET"
+export const pocket = "pocket"
+export const Success = "Success"
+export const Fail = "Fail"
+export const FUNDING = "FUNDING"
+export const account = "account"
+
+
+
