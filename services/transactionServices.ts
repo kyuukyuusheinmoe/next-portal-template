b00a8c12f426a9766baa51c9c2abd4ec4a586bf4
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2024-09-21 10:46:39 +0700

    add cashin service

--- services/transactionServices.ts
+++ services/transactionServices.ts
@@ -1,5 +1,7 @@
 import { BaseService } from "./baseService";
 import { apiRequest } from "./axiosInstance";
+import { TransactionResponse } from "@/app/types/transaciton";
+import { ApiResponse } from '@/app/types/common';
 
 class TransactionService extends BaseService<any,any> {
     constructor () {
@@ -7,7 +9,7 @@ class TransactionService extends BaseService<any,any> {
     }
 
    async cashin() {
-        const res = await apiRequest(this.apiUrl, {method: "POST"})
+        const res = await apiRequest<ApiResponse<TransactionResponse>>(this.apiUrl, {method: "POST"})
 
         if (res.success) {
             return {data: res.data?.data}
