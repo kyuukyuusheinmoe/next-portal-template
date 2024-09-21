3d6eab0d923f50b18b758c105a6bf8c7b7b41d81
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2024-09-21 22:08:22 +0700

    update api request for session expired case

--- services/serveractions.ts
+++ services/serveractions.ts
@@ -2,9 +2,21 @@
 
 import { revalidatePath } from "next/cache"
 import { apiRequest } from './axiosInstance';
-import { ApiResponse, ServiceApiResponse,} from "@/app/types/common";
+import { ApiResponse} from "@/app/types/common";
 import { KeyValueObject } from "@/app/types/common";
-import { TransactionRequest, TransactionResponse } from "@/app/types/transaciton";
+import { TransactionRequest } from "@/app/types/transaciton";
+import {cookies} from 'next/headers'
+import { redirect } from "next/navigation";
+
+const clearCookies = (res: ApiResponse<any>) => {
+  if (res.status === 403) {
+    cookies().set('token', "")
+    cookies().set('user', "")
+    cookies().set('wallet', "")
+    redirect("/auth/signin")
+  }
+}
+
 
 export const get = async <T>(apiUrl:string, queryParams: KeyValueObject) => {
   const query = new URLSearchParams(queryParams as any).toString();
@@ -22,7 +34,7 @@ export const createData = async <T>(apiUrl: string, data: T, pathName?: string):
   return res;
 }
 
-export const patchData = async <T>(apiUrl: string,id: string, data: Partial<T>, pathName?: string): Promise<ServiceApiResponse<T>> => {
+export const patchData = async <T>(apiUrl: string,id: string, data: Partial<T>, pathName?: string): Promise<ApiResponse<T>> => {
   const res = await apiRequest<T>(`${apiUrl}/${id}`, {method: "PATCH", data: data});
 
   if (res.success) {
@@ -33,7 +45,7 @@ export const patchData = async <T>(apiUrl: string,id: string, data: Partial<T>, 
   return res;
 }
 
-export const putData = async <T>(apiUrl: string,id: string, data: Partial<T>, pathName?: string): Promise<ServiceApiResponse<T>> => {
+export const putData = async <T>(apiUrl: string,id: string, data: Partial<T>, pathName?: string): Promise<ApiResponse<T>> => {
   const res = await apiRequest<T>(`${apiUrl}/${id}`, {method: "PUT", data: data});
 
   if (res.success) {
@@ -44,7 +56,7 @@ export const putData = async <T>(apiUrl: string,id: string, data: Partial<T>, pa
   return res;
 }
 
-export const deleteData = async <T>(apiUrl: string, id:string, pathName?: string): Promise<ServiceApiResponse<T>> => {
+export const deleteData = async <T>(apiUrl: string, id:string, pathName?: string): Promise<ApiResponse<T>> => {
   const res = await apiRequest<T>(`${apiUrl}/${id}`, {method: "DELETE"});
 
   if (res.success) {
@@ -56,12 +68,20 @@ export const deleteData = async <T>(apiUrl: string, id:string, pathName?: string
  
 }
 
-export const getDetailsData = async <T>(apiUrl: string, id:string): Promise<ServiceApiResponse<T>> => {
+export const getDetailsData = async <T>(apiUrl: string, id:string): Promise<ApiResponse<T>> => {
   return await apiRequest<T>(`${apiUrl}/${id}`, {method: "GET"});
 
 }
 
-export const cashin = async <T>( data: TransactionRequest ): Promise<ServiceApiResponse<T>> => {
-  return await apiRequest('/api/admin/cashIn', {method: "post", data })
+export const cashin = async <T>( data: TransactionRequest ): Promise<ApiResponse<T>> => {
+  const res = await apiRequest<T>('/api/admin/cashIn', {method: "post", data })
+  clearCookies(res)
+  return res;
+}
+
+export const cashout = async <T>( data: TransactionRequest ): Promise<ApiResponse<T>> => {
+  const res = await apiRequest<T>('/api/admin/cashOut', {method: "post", data })
+  clearCookies(res)
+  return res;
 }
    
