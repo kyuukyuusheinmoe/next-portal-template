252748954f44f006240358626697d4c56965fa25
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2024-09-21 11:51:01 +0700

    add toast context

--- app/containers/MainContainer.tsx
+++ app/containers/MainContainer.tsx
@@ -1,11 +1,14 @@
 "use client"
 import React, { ReactNode } from 'react'
+import { ToastProvider } from '../contexts/ToastContext'
 
 const MainContainer = ({children}: {children: ReactNode}) => {
   return (
+    <ToastProvider>
         <div className="absolute md:ml-[20rem] top-[4rem] p-4 w-full md:max-w-[calc(100%-20rem)] bg-slate-200 min-h-screen h-full overflow-y-scroll hide-scrollbar">
             {children}
         </div>
+    </ToastProvider>
   )
 }
 
---
+++ app/contexts/ToastContext.tsx
@@ -0,0 +1,46 @@
+"use client"
+import { createContext, ReactNode, useState, useEffect } from "react";
+import clsx from 'clsx'
+
+type ToastTypes =  "success" | "error" | "info" ;
+type ToastValueProps = {type: ToastTypes, text: string|null}
+
+type ToastContextProps = {
+    updateToastValue: (value:ToastValueProps) => void
+}
+
+export const ToastContext = createContext<ToastContextProps>({updateToastValue: (undefined) => null})
+
+const toastColorMapper = (type: ToastTypes) => {
+    switch(type) {
+        case "info": return "bg-[#D1ECF1]";
+        case "success": return 'bg-[#D4EDDA]';
+        case "error": return 'bg-[#F8D7DA]'
+    }
+
+}
+
+export const ToastProvider = ({children}: {children: ReactNode}) => {
+    const [toastValue, setToastValue] = useState<ToastValueProps | null>(null);
+
+    const toastType = toastValue?.type ? toastColorMapper(toastValue.type) : ""
+    useEffect(()=> {
+        if (toastValue) {
+            setTimeout(()=> {
+                setToastValue(null)
+            }, 3000)
+        }
+    }, [toastValue])
+
+    const updateToastValue = (value: ToastValueProps) => {
+        setToastValue(value)
+    }
+    return (<ToastContext.Provider value={{updateToastValue}}> 
+                {toastValue && <div className="w-full max-w-sm fixed top-0 z-[50] toast toast-top toast-end">
+                    <div className={clsx("alert rounded-sm", toastType)}>
+                        <span>{toastValue.text}</span>
+                    </div>
+                </div>}
+                {children}
+            </ToastContext.Provider>)
+}
